2449cfe59803fcfd5611d81879c47fcdf358700c
carlsimpson <csimpson.web@gmail.com>
2018-01-30 15:13:17 +0000

    update contact to contain correct email

--- php/contact.php
+++ php/contact.php
@@ -38,7 +38,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "joe.doe@yourdomain.com";
+$address = "carl.simpson@gmail.com";
 
 
 // Configuration option.
